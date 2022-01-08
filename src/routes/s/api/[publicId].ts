eadd8b88635c60017adbd5f3d9600328eb253de4
shajidhasan <shajid6063@gmail.com>
2022-01-09 03:41:33 +0600

    replace \n with new lines

--- src/routes/s/api/[publicId].ts
+++ src/routes/s/api/[publicId].ts
@@ -4,7 +4,7 @@ import admin, { ServiceAccount } from "firebase-admin";
 const serviceAccount: ServiceAccount = {
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 };
 
 if (admin.apps.length === 0) {
